d748993ca8f8ebfddeb1d889849143ef646cdf88
Michael Moerz <michael@natoka.de>
2017-06-07 21:49:37 +0200

    + rename terminator config stuff + make terminator stuff work + provide a little better vim config

--- terminator/install.sh
+++ terminator/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-TERMINATOR_CFGDIR=$HOME/.config/terminator/config
+TERMINATOR_CFGDIR=$HOME/.config/terminator
+test -z "$KEEP_TERMINATOR" || exit 0
 
 mkdir -p $TERMINATOR_CFGDIR 
-cp $DOTFILES/terminator/terminator.config $TERMINATOR_CFGDIR/.
+ln -sf "$DOTFILES/terminator/config" "$TERMINATOR_CFGDIR/config"
