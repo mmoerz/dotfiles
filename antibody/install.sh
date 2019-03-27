8b150631d734ac4ec4c01817436546f52d23ccb1
mmoerz <michael@moerz.co.at>
2019-03-27 16:02:53 +0100

    + adding better antibody installation + adding changed vundle file?

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,7 +3,8 @@ if which antibody > /dev/null 2>&1; then
   echo antibody install
 else
   # install pacman on archlinux
-  if [ $UID -eq 0 ]; then
+  curl -sL https://git.io/antibody | sh -s
+  if [ "X$UID" = "X0" ]; then
 	  pacman -S antibody
   else
 	  sudo pacman -S antibody
@@ -13,3 +14,4 @@ fi
 antibody bundle < "$DOTFILES/antibody/bundles.txt" > ~/.bundles.txt
 antibody bundle sindresorhus/pure >> ~/.bundles.txt
 antibody bundle < "$DOTFILES/antibody/last_bundles.txt" >> ~/.bundles.txt
+antibody update
