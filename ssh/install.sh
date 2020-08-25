9646ea8ab8b36e89239226ffab53834ccb9f0574
mmoerz <michael@moerz.co.at>
2020-08-25 20:22:59 +0200

    install bugfix

--- ssh/install.sh
+++ ssh/install.sh
@@ -11,11 +11,15 @@ if [ ! -L "$USER_SYSTEMD_CFGDIR/ssh-agent.service" ] ; then
     "$USER_SYSTEMD_CFGDIR/ssh-agent.service"
 fi
 
-COUNT=$(grep -c "^SSH_AUTH_SOCK DEFAULT" ~/.pam_environment)
-if [ $COUNT -gt 0 ]; then
+if [ -e ~/.pam_environment ]; then
+  COUNT=$(grep -c "^SSH_AUTH_SOCK DEFAULT" ~/.pam_environment) 
+else
+  COUNT=0
+fi
+if [ $COUNT -gt 0 ] ; then
   sed -i -e \
     's%\(SSH_AUTH_SOCK DEFAULT\).*%\1="\${XDG_RUNTIME_DIR}/ssh-agent.socket"%' \
-    ~/.pam_environment
+      ~/.pam_environment
 #  awk '!(/^SSH_AUTH_SOCK DEFAULT=.*$/ && ++cnt>=2)' ~/.pam_environment
 else 
   echo 'SSH_AUTH_SOCK DEFAULT="${XDG_RUNTIME_DIR}/ssh-agent.socket"' >> \
