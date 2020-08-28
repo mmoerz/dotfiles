0f7bd114b82ecbe86a77acde63c42106a1ccbfc0
mmoerz <michael@moerz.co.at>
2020-08-28 21:40:08 +0200

    hmm, delete ?

--- irssi/install.sh
+++ irssi/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ ! -f $HOME/.irssi/sasl.auth ]; then
+SASL_AUTH_FILE=$DOTFILES/irssi/irssi.symlink/sasl.auth
+
+if [ ! -f $SASL_AUTH_FILE ]; then
   echo -ne "irssi username default is [$USER]:"
-  read $IRSSIUSER
+  read IRSSIUSER
   if [ "X$IRSSIUSER" == "X" ]; then
-    $IRSSIUSER=$USER
+    IRSSIUSER=$USER
   fi
   echo -ne "irssi password []:"
-  read $PASS
-  cp $DOTFILES/irssi/sasl.auth $DOTFILES/irssi/irssi.symlink/.
-  sed -ie 's/_REPLACE_USER_/$IRSSIUSER/' -e 's/_REPLACE_PASS_/$PASS/' \
-    $DOTFILES/irssi/irssi.symlink/sasl.auth
+  read PASS
+  cp $DOTFILES/irssi/sasl.auth $SASL_AUTH_FILE
+  sed -ie "s/_REPLACE_USER_/$IRSSIUSER/" $SASL_AUTH_FILE
+  sed -ie "s/_REPLACE_PASS_/$PASS/" $SASL_AUTH_FILE
 fi
