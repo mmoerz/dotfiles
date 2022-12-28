eae4d60d0e02297148ad3a5f9368c0c31a3517a7
Michael Moerz <michael@moerz.co.at>
2022-12-28 18:49:23 +0100

    ssh-agent socket bugfix - replaced pam environment with zsh export

--- ssh/install.sh
+++ ssh/install.sh
@@ -11,20 +11,24 @@ if [ ! -L "$USER_SYSTEMD_CFGDIR/ssh-agent.service" ] ; then
     "$USER_SYSTEMD_CFGDIR/ssh-agent.service"
 fi
 
-if [ -e ~/.pam_environment ]; then
-  COUNT=$(grep -c "^SSH_AUTH_SOCK DEFAULT" ~/.pam_environment) 
-else
-  COUNT=0
-fi
-if [ $COUNT -gt 0 ] ; then
-  sed -i -e \
-    's%\(SSH_AUTH_SOCK DEFAULT\).*%\1="\${XDG_RUNTIME_DIR}/ssh-agent.socket"%' \
-      ~/.pam_environment
-#  awk '!(/^SSH_AUTH_SOCK DEFAULT=.*$/ && ++cnt>=2)' ~/.pam_environment
-else 
-  echo 'SSH_AUTH_SOCK DEFAULT="${XDG_RUNTIME_DIR}/ssh-agent.socket"' >> \
-    ~/.pam_environment
-fi
+# oh yeah, .pam_enviroment is disabled (and will be removed)
+# cause security concerns ...
+#
+
+#if [ -e ~/.pam_environment ]; then
+#  COUNT=$(grep -c "^SSH_AUTH_SOCK DEFAULT" ~/.pam_environment) 
+#else
+#  COUNT=0
+#fi
+#if [ $COUNT -gt 0 ] ; then
+#  sed -i -e \
+#    's%\(SSH_AUTH_SOCK DEFAULT\).*%\1="\${XDG_RUNTIME_DIR}/ssh-agent.socket"%' \
+#      ~/.pam_environment
+##  awk '!(/^SSH_AUTH_SOCK DEFAULT=.*$/ && ++cnt>=2)' ~/.pam_environment
+#else 
+#  echo 'SSH_AUTH_SOCK DEFAULT="${XDG_RUNTIME_DIR}/ssh-agent.socket"' >> \
+#    ~/.pam_environment
+#fi
 
 systemctl enable --user ssh-agent.service
 systemctl start --user ssh-agent.service
