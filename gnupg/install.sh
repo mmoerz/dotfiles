d7417e92506c4a47b4a7098554154de1df3d8a5e
Michael Moerz <michael@moerz.co.at>
2025-04-10 21:07:29 +0200

    adding gnupg-agent, added potential faster high-lighting

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -1,8 +1,10 @@
+#/bin/bash
 GNUPGDIR="$HOME/.gnupg"
 if [[ ! -d "$GNUPGDIR" ]]; then 
   addgnupghome $USER
   if [[ ! -d "$GNUPGDIR" ]]; then
     mkdir "$GNUPGDIR"
   fi
-  rm -f $GNUPGDIR/pubring.kbx
+  rm -f "$GNUPGDIR"/pubring.kbx
 fi
+ln -s "$DOTFILES"/gnupg/gpg-agent.conf "$GNUPGDIR"/gpg-agent.conf
