417a7f0f7f78dcaaaab4ad801f0ed935e5453f51
Michael Moerz <michael@moerz.co.at>
2025-04-22 23:09:50 +0200

    fix

--- snap/env.sh
+++ snap/env.sh
@@ -3,4 +3,6 @@ if [[ -d /var/lib/snapd/snap/bin ]] ; then
 fi
 if [[ -e /var/lib/snapd/snap/k9s/current/bin ]] ; then 
 	export PATH=$PATH:/var/lib/snapd/snap/k9s/current/bin
+else 
+	echo not found /var/lib/snapd/snap/k9s/current/bin/
 fi
