076eb7b98fd0cdf14f6fbffb614c631ae84ebe09
Michael Moerz <michael@moerz.co.at>
2025-04-23 11:55:53 +0200

    adding kubeseal downloader to install

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 
 # Set this to, for example, KUBESEAL_VERSION='0.23.0'
-KUBESEAL_VERSION='0.29'
-curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
+KUBESEAL_VERSION='0.28'
+
+# Fetch the latest sealed-secrets version using GitHub API
+KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
+
+# Check if the version was fetched successfully
+if [ -z "$KUBESEAL_VERSION" ]; then
+    echo "Failed to fetch the latest KUBESEAL_VERSION"
+    exit 1
+fi
+
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
 tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
