6e6a8589f76f1669553e3fda01f8ecca9cc97ea4
Michael Moerz <michael@moerz.co.at>
2025-04-23 11:42:54 +0200

    correcting gnupg so that it starts only if private key is found

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -12,7 +12,8 @@ ln -s "$DOTFILES"/gnupg/gpg-agent.conf "$GNUPGDIR"/gpg-agent.conf
 
 if [[ -z $( ls -A "$GNUPGDIR"/private-keys-v1.d) ]]; then
   echo "need to generate gpg keys next> gpg --full-gen-key --expert"
-fi
-systemctl --user enable gpg-agent.service gpg-agent.socket \
+else
+  systemctl --user enable gpg-agent.service gpg-agent.socket \
   gpg-agent-ssh.socket gpg-agent-extra.socket gpg-agent-browser.socket
+fi
 
