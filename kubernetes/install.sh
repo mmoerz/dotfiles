e68848970eb78a4bf24fff2988db0f1d1505516a
Michael Moerz <michael@moerz.co.at>
2025-04-23 11:41:19 +0200

    adding kubernetes stuff

---
+++ kubernetes/install.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Set this to, for example, KUBESEAL_VERSION='0.23.0'
+KUBESEAL_VERSION='0.29'
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
--- snap/path.zsh
+++ snap/path.zsh
@@ -1,8 +1,8 @@
 if [[ -d /var/lib/snapd/snap/bin ]] ; then
-	export PATH=$PATH:/var/lib/snapd/snap/bin
+  export PATH=$PATH:/var/lib/snapd/snap/bin
 fi
 if [[ -e /var/lib/snapd/snap/k9s/current/bin ]] ; then 
-	export PATH=$PATH:/var/lib/snapd/snap/k9s/current/bin
+  export PATH=$PATH:/var/lib/snapd/snap/k9s/current/bin
 else 
-	echo not found /var/lib/snapd/snap/k9s/current/bin/
+  echo not found /var/lib/snapd/snap/k9s/current/bin/
 fi
