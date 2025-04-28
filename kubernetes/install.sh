bc2007c5b7e55c9f8ba50f5e4e003d0a4c4f1ae6
Michael Moerz <michael@moerz.co.at>
2025-04-28 20:48:58 +0200

    adding talosctl for talos linux

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# set talos version
+TALOS_VERSION="v1.10.0-beta.1"
+
 # Set this to, for example, KUBESEAL_VERSION='0.23.0'
 KUBESEAL_VERSION='0.28'
 
@@ -29,4 +32,40 @@ if [ "X$FETCH" == "X1" ] ; then
   tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
   sudo install -m 755 kubeseal /usr/local/bin/kubeseal
   rm kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
+fi
+
+TALOSCTL=`which talosctl`
+if [ "X$TALOSCTL" == "X" ] ; then
+  FETCH=1
+else
+  VERSION=`$TALOSCTL --version`
+  if [ "X$VERSION" != "X$TALOS_VERSION" ] ; then
+    echo "installed verson:$VERSION, changin to $TALOS_VERSION"
+    FETCH=1
+  fi
+fi
+
+if [ "X$FETCH" == "X1" ] ; then
+
+  if [ -e ~/Downloads/metal-amd64-${TALOS_VERSION}.iso ] ; then
+    echo "iso downloaded already, skipping"
+  else
+    curl -OL "https://github.com/siderolabs/talos/releases/download/${TALOS_VERSION}/metal-amd64.iso"
+  fi
+  curl -OL "https://github.com/siderolabs/talos/releases/download/${TALOS_VERSION}/talosctl-linux-amd64"
+  curl -OL "https://github.com/siderolabs/talos/releases/download/${TALOS_VERSION}/sha256sum.txt"
+
+  sha256sum --ignore-missing -c sha256sum.txt
+  if [ $? -ne 0 ]; then
+    echo sha256sum check failed exiting
+    exit 128
+  fi
+
+
+  [ -e metal-amd64.iso ] && \
+    mv metal-amd64.iso ~/Downloads/metal-amd64-${TALOS_VERSION}.iso
+  chmod +x talosctl-linux-amd64
+  sudo mv talosctl-linux-amd64 /usr/local/bin/talosctl
+  sudo chown root:root /usr/local/bin/talosctl
+
 fi
