9b0f13f91e379958f3433a29ca8ea58280041e33
Michael Moerz <michael@moerz.co.at>
2026-06-17 21:14:06 +0200

    improved aliases for kubernetes

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,4 +1,9 @@
 #!/bin/sh
-alias kctl='kubectl --kubeconfig .kube/config'
-alias kcgn="kubectl --kubeconfig .kube/config get nodes"
-alias kcci="kubectl --kubeconfig .kube/config cluster-info"
+alias kctl='kubectl --kubeconfig ~/.kube/config'
+alias kcgn="kubectl --kubeconfig ~/.kube/config get nodes"
+alias kcci="kubectl --kubeconfig ~/.kube/config cluster-info"
+alias kpods="kubectl --kubeconfig ~/.kube/config get pods --all-namespaces"
+
+alias flux-reconcile="flux reconcile source git flux-system" 
+alias flux-log="flux logs"
+
