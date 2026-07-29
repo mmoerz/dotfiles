db72236162e4b51f75513888b53580e70d4768d4
Michael Moerz <michael@moerz.co.at>
2026-07-29 21:34:08 +0200

    fixing vim install breaks loop bug and adding new terminator config and vscodium specific symlink

--- vscodium/install.sh
+++ vscodium/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
 
-VSCODIUM_CFGDIR=$HOME/.config/Code/User
+VSCODE_CFGDIR=$HOME/.config/Code/User
+VSCODIUM_CFGDIR=$HOME/.var/app/com.vscodium.codium/config/VSCodium/User
 CFG=settings.json
+SOURCE=settings.json
 
 if [ ! -e "$VSCODIUM_CFGDIR/$CFG" ]; then
-  ln -sf "$DOTFILES/vscodium/$CFG" "$VSCODIUM_CFGDIR/$CFG"
+  ln -sf "$DOTFILES/vscodium/$SOURCE" "$VSCODIUM_CFGDIR/$CFG"
 else
-  echo "VSCODIUM_CFGDIR/$CFG already exists, remove it for "
-  echo "replacing it with symlink version"
+  echo "\033[0;31m"
+  echo "$VSCODIUM_CFGDIR/$CFG"
+  echo "already exists, remove it for dotfile version symlink"
+  echo "\033[0m"
 fi
